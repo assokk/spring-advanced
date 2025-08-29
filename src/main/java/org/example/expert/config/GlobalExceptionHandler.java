c8e3bcc96212d2ef3b695abdc3f0fad8f5f88f6b
assokk <mijisuu@naver.com>
2025-08-29 20:08:47 +0900

    레벨2-3. refactor: 비밀번호 유효성 검증을 DTO로 이동

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -1,23 +1,34 @@
 package org.example.expert.config;
 
 import org.example.expert.domain.auth.exception.AuthException;
-import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(InvalidRequestException.class)
-    public ResponseEntity<Map<String, Object>> invalidRequestExceptionException(InvalidRequestException ex) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        return getErrorResponse(status, ex.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", 400);
+        body.put("errors", errors);
+
+        return ResponseEntity.badRequest().body(body);
     }
 
     @ExceptionHandler(AuthException.class)
